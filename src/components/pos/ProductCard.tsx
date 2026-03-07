0983534ce42d9cb964b1496c8c1d77e5a37fcaf3
arundhathi6 <mallikarjunharalalli@gmail.com>
2026-03-08 00:22:27 +0530

    feat: introduce ProductCard component to display menu items with cart integration, dynamic descriptions, and category-based styling.

--- src/components/pos/ProductCard.tsx
+++ src/components/pos/ProductCard.tsx
@@ -188,18 +188,18 @@ export function ProductCard({ item }: ProductCardProps) {
 </Lens>
 
     {/* Veg/Non-veg + Category */}
-    <div className="absolute top-3 left-3 right-3 flex justify-between items-center z-20">
-      <div
-        className={`w-5 h-5 rounded flex items-center justify-center border-2 ${
-          item.isVeg ? "border-green-500" : "border-red-500"
-        }`}
-      >
-        <div
-          className={`w-2 h-2 rounded-full ${
-            item.isVeg ? "bg-green-500" : "bg-red-500"
-          }`}
-        />
-      </div>
+    <div className="absolute top-3 left-3 right-3 flex justify-between items-center z-20 ">
+     <div
+  className={`w-5 h-5 rounded flex items-center justify-center border-2 backdrop-blur-md bg-white shadow-sm ${
+    item.isVeg ? "border-green-500" : "border-red-500"
+  }`}
+>
+  <div
+    className={`w-2 h-2 rounded-full ${
+      item.isVeg ? "bg-green-500" : "bg-red-500"
+    }`}
+  />
+</div>
 
       <span
         className={`px-3 py-1 rounded-full text-[10px] font-bold shadow-lg backdrop-blur-sm ${getCategoryColor(
